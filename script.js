ead246e0d7b0ffa92f3047b3bbc2946e4ca85038
Al Berthe <alberthe224@gmail.com>
2025-02-11 22:34:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.onload = function() {
+    // Sélectionner le bouton par son ID et ajouter un événement de clic
+    document.getElementById("myButton").addEventListener("click", function() {
+        alert("Merci pour votre visite !");
+    });
+};
